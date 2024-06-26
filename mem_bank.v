4906424eaae2f16dd0ae360b175dc2c1bb4940fe
Himesh Jha <himeshjha22@gmail.com>
2024-06-26 15:13:36 +0530

    added gate level comparator

--- mem_bank.v
+++ mem_bank.v
@@ -34,38 +34,41 @@ start,data_outw1,data_outw2,data_outw3,data_outx1,data_outx2,data_outx3
     output reg start;
     reg[3:0] w_mem[8:0];
     reg[3:0] x_mem[8:0];
-    integer x=0;
-    integer w=0;
-    integer i;
+    integer x=0;// for address of x
+    integer w=0;// for address of w
+    //integer i;
     output reg[3:0] data_outw1,data_outw2,data_outw3,data_outx1,data_outx2,data_outx3;
-
+    wire x_lim,w_lim;
     parameter S0=2'b00,S1=2'b01,S2=2'b10,S3=2'b11;
+    comparator_greater_than gtw(4'd9,w,w_lim);
+    comparator_greater_than gtx(4'd9,x,x_lim);
+    
 
     initial
         start<=1'b0;
         
-    always@( posedge clk)
+    always@( posedge clk) // data writing
          begin
-            if(load_w&& w<9)
+            if(load_w&& w_lim)
                 begin
                 w_mem[w]=data_in;
                 w=w+1;
                 end
-            else if(load_x && x<9)
+            else if(load_x && x_lim)
                 begin
                 x_mem[x]=data_in;
                 x=x+1; 
                 end   
          end
      always@(posedge clear)
-        begin
+        
             if(clear)
             begin
-               {w_mem[0],w_mem[1],w_mem[2],w_mem[3],w_mem[4],w_mem[5],w_mem[6],w_mem[7],w_mem[8]}=36'b0;
-               {x_mem[0],x_mem[1],x_mem[2],x_mem[3],x_mem[4],x_mem[5],x_mem[6],x_mem[7],x_mem[8]}=36'b0;
+               {w_mem[0],w_mem[1],w_mem[2],w_mem[3],w_mem[4],w_mem[5],w_mem[6],w_mem[7],w_mem[8]}=36'd0;
+               {x_mem[0],x_mem[1],x_mem[2],x_mem[3],x_mem[4],x_mem[5],x_mem[6],x_mem[7],x_mem[8]}=36'd0;
             end        
                 
-        end 
+        
         always@(x)
             begin
                 if(x==8)
