4c5d37ee32919f650a11d2983d36377a9018b8d5
Himesh Jha <himeshjha22@gmail.com>
2024-07-12 11:43:23 +0530

    Update adder .v
    added a 4bit adder

--- adder .v
+++ adder .v
@@ -19,3 +19,12 @@ output sum,cout
     adder q1(hsum,cin,sum,hc2);
     or o(cout,hc1,hc2);
 endmodule
+
+module adder_4bit(output[3:0] out,input[3:0] a,b);
+    wire carries[2:0];
+    fulladder f1(a[0],b[0],1'b0,out[0],carries[0]);
+    fulladder f2(a[1],b[1],carries[0],out[1],carries[1]);
+    fulladder f3(a[2],b[2],carries[1],out[2],carries[2]);
+    fulladder f4(a[3],b[3],carries[2],out[3],out[4]);
+    
+endmodule
