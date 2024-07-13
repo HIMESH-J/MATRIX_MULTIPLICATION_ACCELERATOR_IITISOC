cbe7dd52970788742914f4b1a80a6e0e3058cbb5
Himesh Jha <himeshjha22@gmail.com>
2024-07-13 18:40:38 +0530

    Update datapath.v
    data out port added

--- datapath.v
+++ datapath.v
@@ -1,16 +1,17 @@
-module datapath(data_in,clk,clear_mem,row_w,row_x,col_w,col_x);
+module datapath(data_out,data_in,clk,clear_mem,row_w,row_x,col_w,col_x);
     input[3:0] data_in;
     input clk;
+    output[9:0] data_out;
     
     input clear_mem;
     input [1:0] row_w,row_x,col_w,col_x;
     
     wire[3:0] data_outw1,data_outw2,data_outw3,data_outx1,data_outx2,data_outx3;
-    wire ld_mac,clear_mac;
-    mem_bank b(data_outw1,data_outw2,data_outw3,data_outx1,data_outx2,data_outx3,ld_mac,clear_mac,
+    wire ld_mac,clear_mac,unload_res;
+    mem_bank b(data_outw1,data_outw2,data_outw3,data_outx1,data_outx2,data_outx3,ld_mac,clear_mac,unload_res,
                 data_in,clear_mem,row_w,row_x,col_w,col_x,
                 clk);
-    matrix_multiplier m(data_outw1,data_outw2,data_outw3,data_outx1,data_outx2,data_outx3,clk,ld_mac,clear_mac);        
+    matrix_multiplier m(data_out,data_outw1,data_outw2,data_outw3,data_outx1,data_outx2,data_outx3,clk,ld_mac,clear_mac,unload_res);        
 
 
     endmodule
