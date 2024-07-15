ee5f91e6b425a2232260089edc1d8b6fb57de72c
Himesh Jha <himeshjha22@gmail.com>
2024-07-15 23:43:56 +0530

    Update datapath.v

--- datapath.v
+++ datapath.v
@@ -7,7 +7,8 @@ module datapath(data_out,data_in,clk,clear_mem,row_w,row_x,col_w,col_x);
     input [1:0] row_w,row_x,col_w,col_x;
     
     wire[3:0] data_outw1,data_outw2,data_outw3,data_outx1,data_outx2,data_outx3;
-    wire ld_mac,clear_mac,unload_res;
+    wire[8:0] ld_mac,clear_mac;
+    wire unload_res;
     mem_bank b(data_outw1,data_outw2,data_outw3,data_outx1,data_outx2,data_outx3,ld_mac,clear_mac,unload_res,
                 data_in,clear_mem,row_w,row_x,col_w,col_x,
                 clk);
