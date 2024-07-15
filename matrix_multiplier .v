45536f81f0d63d8b8d936b586df4d646068b2f11
Himesh Jha <himeshjha22@gmail.com>
2024-07-15 23:44:15 +0530

    Update matrix_multiplier .v

--- matrix_multiplier .v
+++ matrix_multiplier .v
@@ -5,22 +5,23 @@ module matrix_multiplier( data_out,data_w1,data_w2,data_w3,data_x1,data_x2,data_
     
     output [9:0] data_out;
     input[3:0] data_w1,data_w2,data_w3,data_x1,data_x2,data_x3;
-    input clk,load,clear;
+    input clk;
+    input [8:0] load,clear;
     input unload_res;
     
     wire[9:0] res[8:0];
     reg[3:0] i=4'd0;
-    MAC m11(data_w1,data_x1,res[0],clk,load,clear);
-    MAC m12(data_w1,data_x2,res[1],clk,load,clear);
-    MAC m13(data_w1,data_x3,res[2],clk,load,clear);
+    MAC m11(data_w1,data_x1,res[0],clk,load[0],clear[0]);
+    MAC m12(data_w1,data_x2,res[1],clk,load[1],clear[1]);
+    MAC m13(data_w1,data_x3,res[2],clk,load[2],clear[2]);
     
-    MAC m21(data_w2,data_x1,res[3],clk,load,clear);
-    MAC m22(data_w2,data_x2,res[4],clk,load,clear);
-    MAC m23(data_w2,data_x3,res[5],clk,load,clear);
+    MAC m21(data_w2,data_x1,res[3],clk,load[3],clear[3]);
+    MAC m22(data_w2,data_x2,res[4],clk,load[4],clear[4]);
+    MAC m23(data_w2,data_x3,res[5],clk,load[5],clear[5]);
     
-    MAC m31(data_w3,data_x1,res[6],clk,load,clear);   
-    MAC m32(data_w3,data_x2,res[7],clk,load,clear);    
-    MAC m33(data_w3,data_x3,res[8],clk,load,clear);
+    MAC m31(data_w3,data_x1,res[6],clk,load[6],clear[6]);   
+    MAC m32(data_w3,data_x2,res[7],clk,load[7],clear[7]);    
+    MAC m33(data_w3,data_x3,res[8],clk,load[8],clear[8]);
     
     wire select_unload;
     wire i_less_than_9;
